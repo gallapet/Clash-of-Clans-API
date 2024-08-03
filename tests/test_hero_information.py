0477546c424641906cb79cbfbf555e650dd21451
Peter Gallacher <peter.gallacher@hotmail.co.uk>
2024-08-04 00:00:46 +0100

    update tests

--- tests/test_hero_information.py
+++ tests/test_hero_information.py
@@ -5,7 +5,6 @@ import coc_api as ca
 r = open("../response.json")
 test_resp = json.load(r)
 
-
 class TestGetPlayerHeroes(unittest.TestCase):
     def test_player_heroes_correct_length(self):
         self.assertEqual(len(ca.get_player_heroes(test_resp)), 4)
@@ -13,5 +12,6 @@ class TestGetPlayerHeroes(unittest.TestCase):
 class TestBarbarianKing(unittest.TestCase):
     def test_first_hero_is_barbarian_king(self):
         self.assertEqual(ca.get_player_heroes(test_resp)[0].name, "Barbarian King")
+
     def test_barbarian_king_is_maxed(self):
         self.assertEqual(ca.get_player_heroes(test_resp)[0].is_max(), True)
